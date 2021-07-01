9243f789e1401e1168f883e8e8eb9756b21947b3
guliter <86565392+guliter@users.noreply.github.com>
2021-07-01 15:48:24 +0800

    Update centOSbtlnmp.sh

--- centOSbtlnmp.sh
+++ centOSbtlnmp.sh
@@ -65,11 +65,11 @@ init_env(){
     bash /www/server/panel/install/install_soft.sh 1 install apache 2.4.46 &>/dev/null
     c=$(date "+%s")
     echo "Apache安装完成，耗时：$(($c-$b))s"
-    echo "开始安装php7.3"
-    bash /www/server/panel/install/install_soft.sh 1 install php 7.3 &>/dev/null || echo 'Ignore Error' &>/dev/null
+    echo "开始安装php7.1"
+    bash /www/server/panel/install/install_soft.sh 1 install php 7.1 &>/dev/null || echo 'Ignore Error' &>/dev/null
     d=$(date "+%s")
     echo "php安装完成，耗时：$(($d-$c))s"
-    echo "开始安装mysql mariadb_10.3"
+    echo "开始安装mysql 5.6"
     bash /www/server/panel/install/install_soft.sh 1 install mysql 5.6 &>/dev/null
     e=$(date "+%s")
     echo "mysql安装完成，耗时：$(($e-$d))s"    
@@ -79,7 +79,7 @@ init_env(){
     echo "phpadmin安装完成，耗时：$(($f-$e))s"
     echo "所有软件已安装完毕"
     #添加软件到首页    
-    echo '["linuxsys", "webssh", "apache", "php-7.3", "mysql", "phpmyadmin"]' > /www/server/panel/config/index.json
+    echo '["linuxsys", "webssh", "apache", "php-7.1", "mysql", "phpmyadmin"]' > /www/server/panel/config/index.json
     echo "正在重启所有服务器组件"
     for file in `ls /etc/init.d`
     do if [ -x /etc/init.d/${file} ];  then 
@@ -145,11 +145,11 @@ init_env(){
     bash /www/server/panel/install/install_soft.sh 1 install nginx 1.17 &>/dev/null
     c=$(date "+%s")
     echo "Apache安装完成，耗时：$(($c-$b))s"
-    echo "开始安装php7.3"
-    bash /www/server/panel/install/install_soft.sh 1 install php 7.3 &>/dev/null || echo 'Ignore Error' &>/dev/null
+    echo "开始安装php7.1"
+    bash /www/server/panel/install/install_soft.sh 1 install php 7.1 &>/dev/null || echo 'Ignore Error' &>/dev/null
     d=$(date "+%s")
     echo "php安装完成，耗时：$(($d-$c))s"
-    echo "开始安装mysql mariadb_10.3"
+    echo "开始安装mysql 5.6"
     bash /www/server/panel/install/install_soft.sh 1 install mysql 5.6 &>/dev/null
     e=$(date "+%s")
     echo "mysql安装完成，耗时：$(($e-$d))s"    
@@ -159,7 +159,7 @@ init_env(){
     echo "phpadmin安装完成，耗时：$(($f-$e))s"
     echo "所有软件已安装完毕"
     #添加软件到首页    
-    echo '["linuxsys", "webssh", "apache", "php-7.3", "mysql", "phpmyadmin"]' > /www/server/panel/config/index.json
+    echo '["linuxsys", "webssh", "apache", "php-7.1", "mysql", "phpmyadmin"]' > /www/server/panel/config/index.json
     echo "正在重启所有服务器组件"
     for file in `ls /etc/init.d`
     do if [ -x /etc/init.d/${file} ];  then 
