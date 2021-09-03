90ce5220dcec4f4b800e5485eb88af33505bdf8b
guliter <86565392+guliter@users.noreply.github.com>
2021-09-03 10:14:58 +0800

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo root:898989. |sudo chpasswd root
+echo root:1314521. |sudo chpasswd root
 sudo sed -i ‘s/^.*PermitRootLogin.*/PermitRootLogin yes/g’ /etc/ssh/sshd_config;
 sudo sed -i ‘s/^.*PasswordAuthentication.*/PasswordAuthentication yes/g’ /etc/ssh/sshd_config;
 sudo reboot
