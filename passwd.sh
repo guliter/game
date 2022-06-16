c12691537c37bc080c8e1f866d186d1909cf5511
guliter <86565392+guliter@users.noreply.github.com>
2022-06-16 20:23:23 +0800

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -2,4 +2,6 @@
 echo root:1314521. |sudo chpasswd root
 sudo sed -i ‘s/^.*PermitRootLogin.*/PermitRootLogin yes/g’ /etc/ssh/sshd_config;
 sudo sed -i ‘s/^.*PasswordAuthentication.*/PasswordAuthentication yes/g’ /etc/ssh/sshd_config;
+clear
+tput setaf 1; echo "密码：1314521."
 sudo reboot
