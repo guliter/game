4d3413c68f9b50a433361a80313fe41a02d2ce7b
guliter <86565392+guliter@users.noreply.github.com>
2022-06-17 17:49:23 +0800

    Update ChangeSource.sh

--- ChangeSource.sh
+++ ChangeSource.sh
@@ -1,3 +1,20 @@
+#! /bin/bash
+# By jcnf
+
+#颜色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+
 wget -O "/root/changesource.sh" "https://raw.githubusercontent.com/Netflixxp/jcnf-box/master/sh/changesource.sh" --no-check-certificate -T 30 -t 5 -d
 chmod +x "/root/changesource.sh"
 chmod 777 "/root/changesource.sh"
