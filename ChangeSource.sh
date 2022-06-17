587941cb38d8ec9154c80ab05623fe7560513f8c
guliter <86565392+guliter@users.noreply.github.com>
2022-06-17 18:00:54 +0800

    Add files via upload

---
+++ ChangeSource.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# By jcnf
+
+#颜色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+wget -O "/root/changesource.sh" "https://raw.githubusercontent.com/Netflixxp/jcnf-box/master/sh/changesource.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/changesource.sh"
+chmod 777 "/root/changesource.sh"
+yellow "下载完成"
+echo
+green "请自行输入下面命令切换对应源"
+green " =================================================="
+echo
+green " bash changesource.sh 切换推荐源 "
+green " bash changesource.sh cn  切换中科大源 "
+green " bash changesource.sh aliyun 切换阿里源 "
+green " bash changesource.sh 163 切换网易源 "
+green " bash changesource.sh aws 切换AWS亚马逊云源 "
+green " bash changesource.sh restore 还原默认源 
