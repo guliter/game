27a39b22af2c69a22afe6aad1da4d3f3f130180c
guliter <86565392+guliter@users.noreply.github.com>
2022-06-19 04:02:54 +0800

    Update 3proxy-socks5.sh

--- 3proxy-socks5.sh
+++ 3proxy-socks5.sh
@@ -66,9 +66,9 @@ socks -p${dk}
 EOF
 systemctl stop 3proxy.service
 systemctl start 3proxy.service
+clear
 netstat -tunlp | grep ${td}
 red "--->3proxy-添加用户:【${uname}】端口:【${dk}】 有效期:【${td}天】操作已执行<---"
-clear
 green "---> 已存在以下端口 <---"
 echo ""
 cat /usr/local/3proxy/conf/3proxy.cfg | sed -n '/socks -p/,/socks -p/p'| sed  -e 's/socks -p//g'
