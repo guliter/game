b0d01c90738ff8938215a11c97fc679666f61173
guliter <86565392+guliter@users.noreply.github.com>
2022-06-20 01:49:53 +0800

    Update 3proxy-socks5.sh

--- 3proxy-socks5.sh
+++ 3proxy-socks5.sh
@@ -152,7 +152,7 @@ stty erase '^H' && read -p "输入【用户限制流量/GB】: " jhh
 sed -i -e '/'${users}'/d' /usr/local/3proxy/conf/counters
 echo ""
 a=$[1024*${jhh}]
-b=awk 'END {print NR}' /usr/local/3proxy/conf/counters
+b=cat /usr/local/3proxy/conf/counters | wc -l
 clear
 green "--->已设置用户【${users}】的流量为【${a}】GB<---"
 cat >> /usr/local/3proxy/conf/counters<<EOF
