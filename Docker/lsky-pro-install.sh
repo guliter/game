d33e187b38e183660ca7fd69b3ae6ae56d3c035b
guliter <86565392+guliter@users.noreply.github.com>
2022-07-06 04:00:29 +0800

    Delete lsky-pro-install.sh

--- Docker/lsky-pro-install.sh
+++
@@ -1,8 +0,0 @@
-yum -y install unzip zip
-mkdir -p /root/data/docker_data/lsky-pro
-wget https://raw.githubusercontent.com/guliter/game/main/Docker/lsky-pro/docker-compose.yml -P /root/data/docker_data/lsky-pro
-
-#chmod 777 /root/data/docker_data/lsky-pro
-echo -e "\033[36m cd /root/data/docker_data/lsky-pro \033[0m"
-echo -e "\033[36m docker-compose up -d \033[0m"
-rm $0
