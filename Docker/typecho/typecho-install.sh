b508bc44d3042515c266a84593d97fab4e7ba46f
guliter <86565392+guliter@users.noreply.github.com>
2022-07-07 16:25:15 +0800

    Update typecho-install.sh

--- Docker/typecho/typecho-install.sh
+++ Docker/typecho/typecho-install.sh
@@ -1,3 +1,34 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+
+# 设置字体颜色函数
+function blue(){
+    echo -e "\033[34m\033[01m $1 \033[0m"
+}
+function green(){
+    echo -e "\033[32m\033[01m $1 \033[0m"
+}
+function greenbg(){
+    echo -e "\033[43;42m\033[01m $1 \033[0m"
+}
+function red(){
+    echo -e "\033[31m\033[01m $1 \033[0m"
+}
+function redbg(){
+    echo -e "\033[37;41m\033[01m $1 \033[0m"
+}
+function yellow(){
+    echo -e "\033[33m\033[01m $1 \033[0m"
+}
+function white(){
+    echo -e "\033[37m\033[01m $1 \033[0m"
+}
+
+ip=`curl http://whatismyip.akamai.com`
+
+
 yum -y install unzip zip
 mkdir -p /root/data/docker_data/typecho
 wget https://raw.githubusercontent.com/DDSRem/typecho-install/main/mysql.env -P /root/data/docker_data/typecho
@@ -13,4 +44,8 @@ wget https://raw.githubusercontent.com/DDSRem/typecho-install/main/docker-compos
 chmod 777 /root/data/docker_data/typecho/typecho/usr/uploads
 echo -e "\033[36m cd /root/data/docker_data/typecho \033[0m"
 echo -e "\033[36m docker-compose up -d \033[0m"
+echo -e "\033[36m docker-compose up -d \033[0m"
+echo
+redbg "【typecho-博客】-默认面板:http://${ip}:8223"
+echo
 rm $0
