4c2ad001dfc4905c25af9291008315e083cdea1e
guliter <86565392+guliter@users.noreply.github.com>
2022-07-10 21:42:39 +0800

    Update docker_control.sh

--- Docker/docker_control.sh
+++ Docker/docker_control.sh
@@ -30,7 +30,7 @@ function white(){
 install_ps(){
 docker ps -a --format "table {{.Names}}" | grep -v  "portainer" | grep -v -n "NAMES"
 echo
-redbg  read -p "输入要重启的容器" restart
+read -p "输入要重启的容器" restart
 docker restart $restart
 echo
 redbg "$restart-容器已重启"
