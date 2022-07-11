5a5c74e9b21a124fe8eed2f2355ceaa5dc0a0924
guliter <86565392+guliter@users.noreply.github.com>
2022-07-11 15:09:19 +0800

    Update docker_control.sh

--- Docker/docker_control.sh
+++ Docker/docker_control.sh
@@ -142,8 +142,8 @@ start_menu(){
 clear
 green "已经运行的容器:"
 echo
-a=docker ps -a --format "table {{.Names}}\t{{.Ports}}" | sed 's/0.0.0.0://' | sed 's/ ::://' |awk -F"/tcp," '{print $1}')
-yellow $a
+b=$(docker ps -a --format "table {{.Names}}\t{{.Ports}}" | sed 's/0.0.0.0://' | sed 's/ ::://' |awk -F"/tcp," '{print $1}')
+yellow $b
     echo
     yellow "Docker版绝对优势：部署多个程序互不干扰，独立运行；部署速度快，维护方便 输入【100】提供更详细内容"
     echo
