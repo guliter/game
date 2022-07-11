bf100bf8a6a908f98c710af2bf0ba8158db53900
guliter <86565392+guliter@users.noreply.github.com>
2022-07-11 16:28:52 +0800

    Update docker_control.sh

--- Docker/docker_control.sh
+++ Docker/docker_control.sh
@@ -153,8 +153,7 @@ docker ps -a --format "table {{.Names}}\t{{.Ports}}" | sed 's/0.0.0.0://' | sed 
     green "1.重启指定容器	3.删除所有镜像	5.查看指定容器进程	7.进入指定容器"		
     green "2.删除指定容器	4.删除所有容器	6.查看指定容器信息	8.容器内存占用"
     echo
-    yellow "mysql-端口:6878| phpmyadmin-端口:8181 | Kodexplorer-端口:5878 | Nginx Proxy Manager-端口:81"
-    green "mysql-端口:6878| phpmyadmin-端口:8181 | Kodexplorer-端口:5878 | Nginx Proxy Manager-端口:81"
+    yellow "mysql-端口:6878| phpmyadmin-端口:8181 | Kodexplorer-端口:5878 | Nginx Proxy Manager-端口:81 | Portainer-端口:9000"
     echo
     read -p "请输入数字:" num
     case "$num" in
