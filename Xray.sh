abe442c88949c6bfd0ebc6bea2e2ce4f18c342a0
guliter <86565392+guliter@users.noreply.github.com>
2022-07-22 03:07:58 +0800

    Update Xray.sh

--- Xray.sh
+++ Xray.sh
@@ -27,7 +27,7 @@ function white(){
 
 ws_port="5466"
 
-yellow "输入端口:" 
+yellow "请在↓输入端口:" 
 printf "(默认端口: \e[33m5466\e[0m):"
 echo
 read ws_porttmp
@@ -41,10 +41,9 @@ fi
 ws_id="e98b29e6-83bb-4128-a439-3d0fcb5738c2"
 
 psd="/proc/sys/kernel/random/uuid"
-green "推荐使用的UUID：$(cat $psd)"
-
-yellow "输入UUID:"
-
+yellow "推荐使用的UUID：$(cat $psd)"
+echo
+yellow "请在↓输入UUID:"
 printf "(默认UUID: \e[33me98b29e6-83bb-4128-a439-3d0fcb5738c2\e[0m): "
 read ws_idtmp
 
