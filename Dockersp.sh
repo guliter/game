3e92381705594e27877f543485b193d4640a6d29
guliter <86565392+guliter@users.noreply.github.com>
2022-07-23 02:21:23 +0800

    Update Dockersp.sh

--- Dockersp.sh
+++ Dockersp.sh
@@ -206,7 +206,7 @@ echo
 
 install_14(){
 docker run --name="ccaa" -d -p 6080:6080 -p 6081:6081 -p 6800:6800 -p 51413:51413 \
-    -v /data/ccaaDown:/data/ccaaDown \
+    -v /root/ccaaDown:/data/ccaaDown \
     -e PASS="xiaoz.me" \
     helloz/ccaa \
     sh -c "dccaa pass && dccaa start"
