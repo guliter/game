70e4d7539d55e971041fabe1ecc238e19a01dc36
guliter <86565392+guliter@users.noreply.github.com>
2022-07-24 02:56:50 +0800

    Update Dockersp.sh

--- Dockersp.sh
+++ Dockersp.sh
@@ -228,7 +228,8 @@ install_16(){
 docker run -d --restart=always -v /root/data/docker_data/alist:/opt/alist/data -p 5244:5244 --name="alist" xhofe/alist:latest  #可以自己保存下来，比如创建一个config.txt的文件，把这条代码复制进去保存，下次换服务器搬家之类的就很容易。
 clear
 pass=$(docker exec -it alist ./alist -password)
-redbg "【AList 网盘】-默认面板:http://$ip:5244 【登录密码：$pass】"
+clear
+redbg "【AList 网盘】-默认面板:http://$ip:5244 $pass"
 redbg "【AList 网盘文档：】https://alist-doc.nn.ci/docs/intro"
 echo
 
