977350995b0ea45ded572441d29726c0f156c911
guliter <86565392+guliter@users.noreply.github.com>
2022-11-11 17:29:22 +0800

    Update install.sh

--- Docker/WordPress/install.sh
+++ Docker/WordPress/install.sh
@@ -26,63 +26,46 @@ function white(){
     echo -e "\033[37m\033[01m $1 \033[0m"
 }
 
-name=zfaka
+
 
 ip=`curl http://whatismyip.akamai.com`
 
-#yum -y install unzip zip
-mkdir -p /root/data/docker_data/$name
 
-wget https://raw.githubusercontent.com/guliter/game/main/Docker/xyfaka/000-default.conf -P /root/data/docker_data/$name
-wget https://raw.githubusercontent.com/guliter/game/main/Docker/xyfaka/apache2.conf -P /root/data/docker_data/$name
-wget https://raw.githubusercontent.com/guliter/game/main/Docker/$name/docker-compose.yml -P /root/data/docker_data/$name
+
+
+
+stty erase '^H' && read -p "网站名称" name
+stty erase '^H' && read -p "网站端口" port     
+mkdir -p /root/data/docker_data/$name
+#wget https://raw.githubusercontent.com/guliter/game/main/Docker/Ubuntu20.04-nginx1.16.1-php7.4.20/nginx.conf -P /root/data/docker_data/$name
+#wget https://raw.githubusercontent.com/guliter/game/main/Docker/Ubuntu20.04-nginx1.16.1-php7.4.20/nginx.conf.default -P /root/data/docker_data/$name
+wget https://raw.githubusercontent.com/guliter/game/main/Docker/Ubuntu20.04-nginx1.16.1-php7.4.20/default_server.conf -P /root/data/docker_data/$name
+
 
 yum -y install unzip zip
-wget https://raw.githubusercontent.com/guliter/game/main/Docker/$name/$name.zip -P /root/data/docker_data/$name
-unzip /root/data/docker_data/$name/$name.zip -d /root/data/docker_data/$name/$name
+wget https://raw.githubusercontent.com/guliter/game/main/Docker/$name/wordpress-6.1-zh_CN.zip -P /root/data/docker_data/$name
+unzip /root/data/docker_data/$name/$wordpress-6.1-zh_CN.zip -d /root/data/docker_data/$name/$name
 
 
-> /root/data/docker_data/$name/$name/conf/application.ini
-cat >> /root/data/docker_data/$name/$name/conf/application.ini<<EOF
-[common]
-application.directory = APP_PATH"/application/"
-application.dispatcher.catchException = 1
-application.cache_config = 1
-application.dispatcher.defaultController = "Index"
-application.dispatcher.defaultAction = "index"
-application.view.ext = "html"
-application.modules = "Index,Member,Product,407413685,Crontab,Install"
-[product : common]
-TYPE = "mysql"
-READ_HOST = "${ip}"
-READ_PORT = 6878
-READ_USER = "root"
-READ_PSWD = root
-WRITE_HOST = "${ip}"
-WRITE_PORT = 6878
-WRITE_USER = "root"
-WRITE_PSWD = root
-Default = "zfaka"
-pconnect = 0
-EOF
 
 chmod -R 777 /root/data/docker_data
-#sed -i '3c $dbconfig=array(' /root/data/docker_data/xyfaka/xyfaka/config.php
-#mkdir -p /root/data/docker_data/xyfaka/xyfaka/install/install.lock
-sed -i '12c DocumentRoot /var/www/html/public' /root/data/docker_data/zfaka/000-default.conf
-
-
-#chmod 777 /root/data/docker_data/xyfaka/xyfaka/install
-#chmod -R 777 /root/data/docker_data/xyfaka/xyfaka/install
-#echo -e "\033[36m cd /root/data/docker_data/lsky-pro \033[0m"
-#echo -e "\033[36m docker-compose up -d \033[0m"
-#rm $0
-
+#sed -i '12c DocumentRoot /var/www/html/public' /root/data/docker_data/$name/000-default.conf
 cd /root/data/docker_data/$name
-redbg "【zfaka】启动中......"
-docker-compose up -d
 echo
-redbg "【zfaka】-默认面板:http://${ip}:5005"
+redbg "【$name】启动中......"
 echo
-redbg "【数据库面板】-默认面板:http://${ip}:8181 【root root】"
+docker run -d \
+  --restart always \
+  --name $name \
+  --link mysql \
+  -p $port:80 \
+  -v /root/data/docker_data/$name/$name:/app/web \
+  -v /root/data/docker_data/$name/default_server.conf:/etc/nginx/conf.d/default_server.conf \
+  ddsderek/foundations:Ubuntu20.04-nginx1.16.1-php7.4.20
+echo
+redbg "【$name-nginx1.16-php7.4环境】-默认面板:http://${ip}:$port"
+echo
+redbg "上传网站至: /root/data/docker_data/$name/$name"
+echo
+redbg "【数据库面板】-默认面板:http://${ip}:8181"
 echo
